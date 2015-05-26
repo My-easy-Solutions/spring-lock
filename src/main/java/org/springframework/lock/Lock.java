c0d81e2834f71c16bbb59c1d2c14273d442661b3
Matthias Augustin <matthias.augustin@my-easy-solutions.com>
2015-05-26 15:29:20 +0200

    fix paths

---
+++ src/main/java/org/springframework/lock/Lock.java
@@ -0,0 +1,23 @@
+package org.springframework.lock;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 
+ * @author matthias.augustin
+ */
+@Target({ ElementType.METHOD, ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface Lock {
+
+   /** Maximum timeout to wait for lock in milleseconds. */
+   long timeout() default 1000L;
+
+   /** Lock identifier. */
+   String value();
+}
---
+++ src/main/java/org/springframework/lock/LockException.java
@@ -0,0 +1,5 @@
+package org.springframework.lock;
+
+public class LockException extends RuntimeException {
+
+}
